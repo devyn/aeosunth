bb41ae989c852411cd1f15dbd746fd2db14eb0e0
devyn <devyn.cairns@gmail.com>
2012-04-23 13:06:16 -0700

    aeosunth-client becomes aeosunth-worker

--- bin/aeosunth-client.hs
+++
@@ -1,7 +0,0 @@
-import Network.IRC.Aeosunth.Client
-import Network.IRC.Aeosunth.Config
-import Network.IRC.Aeosunth.Logger
-          
-main = do args <- getArgs
-          conf <- configFromFile (if null args then "aeosunth-client.yaml" else head args) masterDefaults
-          startClient conf
---
+++ bin/aeosunth-worker.hs
@@ -0,0 +1,7 @@
+import Network.IRC.Aeosunth.Config
+import Network.IRC.Aeosunth.Logger
+import Network.IRC.Aeosunth.Worker
+          
+main = do args <- getArgs
+          conf <- configFromFile (if null args then "aeosunth-worker.yaml" else head args) masterDefaults
+          startWorker conf
