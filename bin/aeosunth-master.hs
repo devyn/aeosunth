56e77c8bead76e374b3f520482ac488076862107
devyn <devyn.cairns@gmail.com>
2012-04-24 23:02:58 -0700

    Migrate bin/ tools to new config module

--- bin/aeosunth-master.hs
+++ bin/aeosunth-master.hs
@@ -1,6 +1,12 @@
-import Network.IRC.Aeosunth.Config
-import Network.IRC.Aeosunth.Master
+import qualified Network.IRC.Aeosunth.Config as Config
+import           Network.IRC.Aeosunth.Master
+import           System.Exit
+import           System.Log (criticalM)
 
-main = do args <- getArgs
-          conf <- configFromFile (if null args then "aeosunth-master.yaml" else head args) masterDefaults
-          startMaster conf
+main = do args    <- getArgs
+          tryConf <- Config.fromFile (if null args then "aeosunth-master.yaml" else head args) masterDefaults
+          case tryConf of
+               Right conf -> startMaster conf
+               Left  err  -> do criticalM "aeosunth.config" $ "Exiting due to configuration parsing failure: " ++ err
+                                exit 1
+
--- bin/aeosunth-worker.hs
+++ bin/aeosunth-worker.hs
@@ -1,6 +1,11 @@
-import Network.IRC.Aeosunth.Config
-import Network.IRC.Aeosunth.Worker
-          
-main = do args <- getArgs
-          conf <- configFromFile (if null args then "aeosunth-worker.yaml" else head args) workerDefaults
-          startWorker conf
+import qualified Network.IRC.Aeosunth.Config as Config
+import           Network.IRC.Aeosunth.Worker
+import           System.Exit
+import           System.Log (criticalM)
+
+main = do args    <- getArgs
+          tryConf <- Config.fromFile (if null args then "aeosunth-worker.yaml" else head args) workerDefaults
+          case tryConf of
+               Right conf -> startWorker conf
+               Left  err  -> do criticalM "aeosunth.config" $ "Exiting due to configuration parsing failure: " ++ err
+                                exit 1
